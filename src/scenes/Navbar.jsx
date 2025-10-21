19023e39923bddee8e8656f1a063cf1713effece
Nasir <nasirsabbir07@gmail.com>
2025-10-21 15:27:53 +0600

    add header section

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -17,10 +17,10 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   );
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreen = useMediaQuery("(min-width: 768px)");
-
+  const navbarBackground = isTopOfPage ? "" : "bg-red";
   return (
     <nav className={`z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
@@ -48,6 +48,28 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
           </button>
         )}
         {/* MOBILE MENU POPUP */}
+        {!isAboveSmallScreen && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img src="../assets/menu-icon.svg" alt="menu-icon" />
+              </button>
+            </div>
+            {/* MENU ITEMS */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              <Link page="Skills" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              <Link page="Projects" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+              <Link
+                page="Testimonials"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link page="Contact" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
